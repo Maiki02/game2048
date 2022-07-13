be0569d7d8c8616ceb03fc1eea73fe0bbb5b04e7
Maiki02 <miqueasdavidgentile@gmail.com>
2022-07-13 14:11:22 -0300

    Se crea el nombre de las funciones para mover el tablero y crearlo

--- src/app/components/game/game.component.ts
+++ src/app/components/game/game.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-
+import { withLatestFrom } from 'rxjs';
+import { Stats } from 'src/app/shared/interfaces/board.interface';
+const ANY_ROW=[0,0,0,0]
+const BOARD_INITIAL=[ANY_ROW,ANY_ROW,ANY_ROW,ANY_ROW];
 @Component({
   selector: 'app-game',
   templateUrl: './game.component.html',
@@ -7,12 +10,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GameComponent implements OnInit {
   private row=[0,0,0,0];
-  public board=[[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]];
+  public stats:Stats={
+    board:[],
+    numOfCols:0,
+    numOfRows:0,
+    score:0
+  };
   constructor() { 
-    console.log("CREANDO GAME")
+    console.log("CREANDO GAME");
+    this.createBoard(2);
   }
 
   ngOnInit(): void {
+  }
+
+  createGame(numOfRows:number){
+    this.stats.numOfCols=numOfRows;
+    this.stats.numOfRows=numOfRows;
+    //this.board
+  }
+
+  //Given a number that represents the number of rows and columns in the board, 
+  //create a board with its initial values set to 0.
+  createBoard(numOfRows:number){
+  }
+
+  moveToDown(){
+    
+  }
+  moveToUp(){}
+  moveToLeft(){}
+  moveToRight(){
+    /*console.log("MOVE TO RIGHT")
+    let boardFinally= BOARD_INITIAL;
+    this.board.forEach(row=>{
+      console.log(row);
+      if(){
+
+      }
+    })*/
+  }
+
+  generateRandom(){
+
   }
 
 }
---
+++ src/app/shared/interfaces/board.interface.ts
@@ -0,0 +1,6 @@
+export interface Stats {
+    board: number[][],
+    numOfRows: number,
+    numOfCols: number,
+    score: number,
+}
